d0bfbe678c9789f64d5ec98e071fdabb60dc49eb
장 상련 <srjang@taihoinst.com>
2018-12-06 10:09:08 +0900

    Luis_before[i]["entities"].Count() 수정

--- DB/DButil.cs
+++ DB/DButil.cs
@@ -99,27 +99,29 @@ namespace cjEmployeeChatBot.DB
                     for (int i = 0; i < MAX; i++)
                     {
                         //if (Luis_before[i]["intents"][0]["intent"].ToString() != "None")
-                        if (Luis_before[i]["intents"][0]["intent"].ToString() != "None" || (int)Luis_before[i]["entities"].Count() > 0)
+                        if ((int)Luis_before[i]["entities"].Count() > 0)
                         {
-                            if ((float)Luis_before[i]["intents"][0]["score"] > Convert.ToDouble(MessagesController.LUIS_SCORE_LIMIT))
+                            if (Luis_before[i]["intents"][0]["intent"].ToString() != "None")
                             {
-                                if ((float)Luis_before[i]["intents"][0]["score"] > luisScoreCompare)
+                                if ((float)Luis_before[i]["intents"][0]["score"] > Convert.ToDouble(MessagesController.LUIS_SCORE_LIMIT))
                                 {
-                                    LuisName = returnLuisName[i];
-                                    Luis = Luis_before[i];
-                                    luisScoreCompare = (float)Luis_before[i]["intents"][0]["score"];
-                                    Debug.WriteLine("GetMultiLUIS() LuisName1 : " + LuisName);
-                                }
-                                else
-                                {
-                                    //LuisName = returnLuisName[i];
-                                    //Luis = Luis_before[i];
-                                    Debug.WriteLine("GetMultiLUIS() LuisName2 : " + LuisName);
-                                }
+                                    if ((float)Luis_before[i]["intents"][0]["score"] > luisScoreCompare)
+                                    {
+                                        LuisName = returnLuisName[i];
+                                        Luis = Luis_before[i];
+                                        luisScoreCompare = (float)Luis_before[i]["intents"][0]["score"];
+                                        Debug.WriteLine("GetMultiLUIS() LuisName1 : " + LuisName);
+                                    }
+                                    else
+                                    {
+                                        //LuisName = returnLuisName[i];
+                                        //Luis = Luis_before[i];
+                                        Debug.WriteLine("GetMultiLUIS() LuisName2 : " + LuisName);
+                                    }
 
+                                }
                             }
-                        }
-                        
+                        }   
                     }
 
                     Debug.WriteLine("luisScoreCompare : " + luisScoreCompare);
