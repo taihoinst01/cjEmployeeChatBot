a280a0ff145d11da406a51fa9659b1bebf7cfef0
장 상련 <srjang@taihoinst.com>
2018-12-14 00:48:38 +0900

    수정

--- App_Start/Controllers/MessagesController.cs
+++ App_Start/Controllers/MessagesController.cs
@@ -273,20 +273,22 @@ namespace cjEmployeeChatBot
                     //취소 시간 설정
                     //string url = "https://cjemployeeconnect.azurewebsites.net/?user_id=" + cjValue_result[0];
                     string url = "https://cjemployeeconnect.azurewebsites.net/";
-                    client.Timeout = TimeSpan.FromMilliseconds(5000); //5초
+                    client.Timeout = TimeSpan.FromMilliseconds(1000); //5초
                     var cts = new CancellationTokenSource();
-                    //try
-                    //{
-                    //    HttpResponseMessage msg = await client.GetAsync(url, cts.Token);
-                    //    var request_msg = await msg.Content.ReadAsStringAsync();
-                    //    Debug.WriteLine("msg=====" + request_msg);
-                    //    DButil.HistoryLog("msg=====" + request_msg);
-                    //}
-                    //catch (Exception ex)
-                    //{
-                    //    Debug.WriteLine("ex.Message=====" + ex.Message);
-                    //    DButil.HistoryLog("ex.Message=====" + ex.Message);
-                    //}
+                    try
+                    {
+                        DButil.HistoryLog("ssoa=====2");
+                        HttpResponseMessage msg = await client.GetAsync(url, cts.Token);
+                        DButil.HistoryLog("ssob=====2");
+                        var request_msg = await msg.Content.ReadAsStringAsync();
+                        Debug.WriteLine("msg=====" + request_msg);
+                        DButil.HistoryLog("msg=====" + request_msg);
+                    }
+                    catch (Exception ex)
+                    {
+                        Debug.WriteLine("ex.Message=====" + ex.Message);
+                        DButil.HistoryLog("ex.Message=====" + ex.Message);
+                    }
                 }
                 Debug.WriteLine("sso=====2");
                 DButil.HistoryLog("sso=====2");
