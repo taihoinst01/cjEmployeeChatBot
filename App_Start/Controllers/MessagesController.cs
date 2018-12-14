ff6730594e0f8d4af9c3bfcf9115e6a6848dbb30
장 상련 <srjang@taihoinst.com>
2018-12-14 19:14:29 +0900

    수정

--- App_Start/Controllers/MessagesController.cs
+++ App_Start/Controllers/MessagesController.cs
@@ -266,7 +266,7 @@ namespace cjEmployeeChatBot
             {
                 DButil.HistoryLog("start sso : ");
                 DButil.HistoryLog("activity.Text sso : " + activity.Text);
-                string sso = dbutil.GetSSO(activity.Text);
+                string sso = dbutil.GetSSO("sso:");
                 DButil.HistoryLog("sso : " + sso);
 
             }
