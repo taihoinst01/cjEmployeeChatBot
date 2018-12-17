d24b5a9a1482d1a2be53aa7318aadd534d254ed9
장 상련 <srjang@taihoinst.com>
2018-12-17 20:30:52 +0900

    수정

--- App_Start/Controllers/MessagesController.cs
+++ App_Start/Controllers/MessagesController.cs
@@ -267,10 +267,10 @@ namespace cjEmployeeChatBot
                 DButil.HistoryLog("start sso : ");
                 //string txt = activity.Text;
                 userID = dbutil.GetSSO(activity.Text);
-                if (string.IsNullOrEmpty(userID))
-                {
-                    userID = "srjang";
-                }
+                //if (string.IsNullOrEmpty(userID))
+                //{
+                //    userID = "srjang";
+                //}
                 db.UserDataUpdateUserID(activity.ChannelId, activity.Conversation.Id, "userid" ,userID);
                 DButil.HistoryLog("sso : " + userID);
 
