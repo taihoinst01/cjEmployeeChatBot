88097784631999063223835ab5d2f11fc5882247
장 상련 <srjang@taihoinst.com>
2018-12-31 11:37:38 +0900

    수정

--- App_Start/Controllers/MessagesController.cs
+++ App_Start/Controllers/MessagesController.cs
@@ -646,7 +646,7 @@ namespace cjEmployeeChatBot
                                         {
                                             //주차신청 get방식 userid 추가
                                             //DButil.HistoryLog("uData[0].userId ==" + uData[0].userId);
-                                            if (tempcard.btn1Context.Contains("http://116.121.31.148/visitor2/menu1.asp?emailAlias="))
+                                            if (!string.IsNullOrEmpty(tempcard.btn1Context) && tempcard.btn1Context.Contains("http://116.121.31.148/visitor2/menu1.asp?emailAlias="))
                                             {
                                                 //DButil.HistoryLog("btn1Context ==1" + tempcard.btn1Context);
                                                 tempcard.btn1Context += uData[0].userId;
